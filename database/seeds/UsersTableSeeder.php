bdffc83dda6b44ffe7d662c052fec9431a527d0d
kurosay <kurosay@gmail.com>
2015-11-28 12:56:45 +0100

    Fix UsersTableSeeder for Postgres

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,8 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin@example.org',
             'password' => bcrypt('secret'),
             'rights' => '["admin","beyer","letras","dommer"]',
+            'created_at' => '2015-11-28 11:49:23',
+            'updated_at' => '2015-11-28 11:49:23',
         ]);
     }
 }
