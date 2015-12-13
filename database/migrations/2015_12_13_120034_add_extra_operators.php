576f3543d2ab4188bdeb1145b57af6ccbd55afe4
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2015-12-13 13:09:25 +0100

    Add postgres json operators that doesn't conflict with PDO

---
+++ database/migrations/2015_12_13_120034_add_extra_operators.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddExtraOperators extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /**
+         * Problem: The question mark operators can't be used in parameterised queries.
+         * Solution: Define new operators that can be used in parameterised queries.
+         *
+         * Source: <http://stackoverflow.com/a/30464989/489916>
+         */
+        DB::statement('CREATE OPERATOR ~@ (LEFTARG = jsonb, RIGHTARG = text, PROCEDURE = jsonb_exists)');
+        DB::statement('CREATE OPERATOR ~@| (LEFTARG = jsonb, RIGHTARG = text[], PROCEDURE = jsonb_exists_any)');
+        DB::statement('CREATE OPERATOR ~@& (LEFTARG = jsonb, RIGHTARG = text[], PROCEDURE = jsonb_exists_all)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP OPERATOR ~@& (jsonb, text[])');
+        DB::statement('DROP OPERATOR ~@| (jsonb, text[])');
+        DB::statement('DROP OPERATOR ~@ (jsonb, text)');
+    }
+}
