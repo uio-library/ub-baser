cb1104830e8e17eb723e39f967fc5aad4de74b34
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2016-01-23 12:39:49 +0100

    Add Content Security Policy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,5 +31,6 @@ class Kernel extends HttpKernel
         'auth'       => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest'      => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'secure.content' => \Stevenmaguire\Laravel\Http\Middleware\EnforceContentSecurity::class,
     ];
 }
