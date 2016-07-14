8c6cb520d0dad18965ee7374516102c8f9d9e569
andrea gasparini <a.a.gasparini@ub.uio.no>
2016-07-14 10:49:45 +0200

    endret opescontroller

--- app/Http/Controllers/OpesController.php
+++ app/Http/Controllers/OpesController.php
@@ -13,7 +13,77 @@ class OpesController extends RecordController
      */
     public function index()
     {
-        //
+    {
+        list($fields, $fieldPairs) = $this->parseFields($request);
+
+        $minYear = '1789';
+        $maxYear = strftime('%Y');
+        $dateRange = [$minYear, $maxYear];
+
+        /*$inputDateRange = preg_split('/[-:,]/', $request->get('date', ''));
+        if (count($inputDateRange) == 2 && strlen($inputDateRange[0]) == 4 && strlen($inputDateRange[1]) == 4) {
+            $dateRange = $inputDateRange;
+        }
+        */
+        $records = OpesRecordView::query();
+
+        /*if ($dateRange[0] != $minYear || $dateRange[1] != $maxYear) {
+            $records->where('aar_numeric', '>=', intval($dateRange[0]))
+                ->where('aar_numeric', '<=', intval($dateRange[1]));
+        }*/
+
+        if (array_has($fields, 'q')) {
+            $term = $fields['q']; 
+            echo = "test";
+            $records->whereRaw("tsv @@ plainto_tsquery('simple', '" . pg_escape_string($term) . "')");
+        }
+
+        /* if (array_has($fields, 'person')) {
+            $term = preg_replace('/,/', '', $fields['person']) . '%';
+            $records->whereRaw("tsv_person @@ plainto_tsquery('simple', '" . pg_escape_string($term) . "')");
+        }
+
+        if (array_has($fields, 'kritikktype')) {
+            $q = $fields['kritikktype'];
+            // Note: The ~@ operator is defined in <2015_12_13_120034_add_extra_operators.php>
+            $records->whereRaw('kritikktype ~@ \'' . pg_escape_string($q) . '\'');
+        }
+
+        if (array_has($fields, 'verk')) {
+            $q = $fields['verk'] . '%';
+            $records->where('verk_tittel', 'ilike', $q);
+        }
+        */
+
+
+        $selectOptions = [
+            ['id' => 'q', 'type' => 'text', 'label' => 'Fritekst', 'placeholder' => 'Forfatter, kritiker, ord i tittel, kommentar, etc...'],
+            ['id' => 'person', 'type' => 'text', 'label' => 'Forfatter eller kritiker', 'placeholder' => 'Fornavn og/eller etternavn'],
+            ['id' => 'verk', 'type' => 'text', 'label' => 'Omtalt tittel', 'placeholder' => 'Verkstittel'],
+            ['id' => 'publikasjon', 'type' => 'select', 'label' => 'Publikasjon', 'placeholder' => 'Publikasjon'],
+            ['id' => 'kritikktype', 'type' => 'select', 'label' => 'Kritikktype', 'placeholder' => 'F.eks. teaterkritikk, forfatterportrett, ...'],
+        ];
+
+        // Make sure there's always at least one input field visible
+        if (!count($fieldPairs)) {
+            $fieldPairs[] = ['q', ''];
+        }
+
+        $intro = Page::where('name', '=', 'litteraturkritikk.intro')->first();
+
+        $records->orderBy('aar_numeric', 'desc');
+
+        $data = [
+            'intro'         => $intro->body,
+            'records'       => $records->paginate(200),
+            'fields'        => $fieldPairs,
+            'selectOptions' => $selectOptions,
+            'date'          => $dateRange,
+            'minDate'       => $minYear,
+            'maxDate'       => $maxYear,
+        ];
+
+        return response()->view('opes.index', $data);
     }
 
     /**
