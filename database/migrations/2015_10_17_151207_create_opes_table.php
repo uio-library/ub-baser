uio-library/ub-baser

commit b17a910f011b5a668abcaa1f508b412487556467

endringer med Dan

database/migrations/2015_10_17_151207_create_opes_table.php
@@ -12,10 +12,34 @@ class CreateOpesTable extends Migration
      */
     public function up()
     {
+        
+
         Schema::create('opes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('navn');
+            $table->integer('aar')->unsigned();
+            $table->integer('kilde_id')->unsigned();
+            $table->integer('side')->unsigned();
+            $table->string('note');
             $table->timestamps();
+            $table->softDeletes(); // blir ikke onterlig slettet - se paa eloquent
+
+            $table->index('navn');
+            $table->index('aar');
+            $table->index('side');
+
+            
+
+            
         });
+
+         /** opes_id i opes_pub sin record maa kunne ha samme verdi    **/
+         Schema::create('opes_pub', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('navn');
+            $table->foreign('opes_id')
+                ->references('id')->on('opes');
+        });  
     }
 
     /**
@@ -25,6 +49,8 @@ class CreateOpesTable extends Migration
      */
     public function down()
     {
+        Schema::drop('opes_pub');
+
         Schema::drop('opes');
     }
 }

app/Http/Controllers/OpesController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\PubOpes;
 use App\OpesRecord;
 use App\Page;
 use App\RecordQueryBuilderOpes;
@@ -8,11 +9,31 @@ use Illuminate\Http\Request;
 
 class OpesController extends RecordController
 {
+    
+    // ny function laget 2 august 2016 
+    // henter ut verdier for alle publication for hver  
+    //  
+    protected function getPublications()
+    {
+        $publi = [];
+        foreach (PubOpes::all() as $publi) {
+            $publi[$publi->id] = $publi->Ser_Vol;
+    
+        }
+    
+        return $publi;
+    }
+
+
+
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+    
+
     public function index(Request $request)
     {
         
@@ -25,32 +46,27 @@ class OpesController extends RecordController
             'sortColumn' => $q->sortColumn,
             'sortOrder' => $q->sortOrder,
         ];
+        // 
+        $data['records'] = OpesRecord::paginate(50);
 
-        $data['records'] = $q->query
-            ->join('opes', 'opes_pub.papy_id', '=', 'opes.id')
-            //->select('opes_pub.*', 'opes.navn AS kilde_navn')
-            // Dan henter verdier     
+        /* $q->query
+          ->join('opes_pub', 'opes.id', '=', 'opes_pub.opes_id')
+          //join('opes', 'opes_pub.papy_id', '=', 'opes.id')
+          ->select('opes_pub.*', 'opes.*')
+          // Dan henter verdier   opes_pub.Ser_Vol 
+         // join('opes_pub', 'opes.id', '=', 'opes_pub.papy_id')
+          // 'opes_pub', 'opes.id', '=', 'opes_pub.papy_id' */
 
 
-        
-            ->paginate(50);
+          // vi vet ikke hva dette er....
+        $data['publikasjoner'] = $this->getPublications();
+
+
         return response()->view('opes.index', $data); 
        
     }
 
-    // ny function laget 2 august 2016 
-    // henter ut verdier for alle publication for hver  
-    //  
-    protected function getPublications()
-    {
-        $publi = [];
-        foreach (PubOpes::all() as $publi) {
-            $publi[$publi->id] = $publi->;
 
-        }
-
-        return $publi;
-    }
 
 
 

app/OpesPublication.php
@@ -1,13 +1,0 @@
-<?php
-
-namespace App;
-
-class OpesPublication extends \Eloquent
-{
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'opes_pub';
-}

app/OpesRecord.php
@@ -19,26 +19,13 @@ class OpesRecord extends Record
 
 // denne henter alle publikasjoner for denne papyrus
 // 2 august 2016
-    public function poster()
+    public function GetPubl()
     {
         return $this->hasMany('App\PubOpes');
     }
 
-    public function inv_no()
-    {
      
-      $repr = $this->inv_no;
-        //
-     return $repr;
-      //$repr = '<a href="' . action('LetrasController@show', $this->id) . '">';
-      //return $repr;
-     //$repr = $this->forfatter;
-        //
-     //return action('LetrasController@show', $this->id);
-    }
-
-     
-
+/*
 
     public function  section_or_side()
     {
@@ -477,7 +464,7 @@ class OpesRecord extends Record
     {
       $repr = $this->id2;
      return $repr;
-    } 
+    } */
 
      
 }

app/PubOpes.php
@@ -17,7 +17,7 @@ class PubOpes extends \Eloquent
     // peker tilbake til papyrus item
     public function papyrus()
     {
-        return $this->belongsTo('App\OpesRecord')
+        return $this->belongsTo('App\OpesRecord');
     }
 
     public function Ser_vol()
@@ -33,6 +33,11 @@ class PubOpes extends \Eloquent
     public function Year()
     {
         return $this->Year;
+    }
+
+     public function __toString()
+    {
+        return $this->Ser_vol;
     }
 
 

resources/views/opes/index.blade.php
@@ -79,11 +79,14 @@ index.blade.php
                 <td>
                     <a href="{{ action('OpesController@show', $record->id) }}">
 
-                        {!! $record->inv_no() !!}
+                    {{$record}}
+
+
+                        {{ $record->inv_no }}
                     </a>
                 </td>
                 <td>
-                        {!! $record->type_of_text_file() !!}
+                        {{ $record->type_of_text_file }}
                 </td>
                 
     </tr>

resources/views/opes/show.blade.php
@@ -33,4 +33,8 @@
 
         </p>
 
+        <h2>
+            Post {{$record->GetPubl}}
+        </h2>
+
 @endsection

commit 1a39b185f9f85e3351202bd0af9a4276632f7e3c

Fix opes table creation migration
Bug introduced in b17a910f

database/migrations/2015_10_17_151207_create_opes_table.php
@@ -12,8 +12,6 @@ class CreateOpesTable extends Migration
      */
     public function up()
     {
-        
-
         Schema::create('opes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('navn');
@@ -27,19 +25,16 @@ class CreateOpesTable extends Migration
             $table->index('navn');
             $table->index('aar');
             $table->index('side');
-
-            
-
-            
         });
 
          /** opes_id i opes_pub sin record maa kunne ha samme verdi    **/
          Schema::create('opes_pub', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('opes_id')->unsigned();
             $table->string('navn');
             $table->foreign('opes_id')
                 ->references('id')->on('opes');
-        });  
+        });
     }
 
     /**

database/migrations/2015_10_17_151212_create_opes_pub_table.php
@@ -1,30 +1,0 @@
-<?php
-
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
-class CreateOpesPubTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('opes_pub', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('opes_pub');
-    }
-}