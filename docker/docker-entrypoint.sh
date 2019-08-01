8fd688da004e7cad4eb863b49b5fd02d909d99dd
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-08-01 21:21:11 +0200

    Move framework optimization into initialization script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -17,6 +17,15 @@ echo "Storing certificates in $APACHE_CONFDIR"
 echo -e $SITE_CERTIFICATE > $APACHE_CONFDIR/site.crt
 echo -e $SITE_CERTIFICATE_KEY > $APACHE_CONFDIR/site.key
 
+# Optimize Laravel. These commands depend on the environment,
+# so we cannot run them in the Dockerfile.
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+
+# Sanity check
+chmod -R a+rX .
+
 if [ "$APP_ENV" == "production" ]; then
 	echo "Copying opcache settings into $PHP_INI_DIR/conf.d/"
 	cp docker/opcache.ini $PHP_INI_DIR/conf.d/
