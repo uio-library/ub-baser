36e0ff0fdea87a776587e5c3edf57e11a3a7cf50
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-08-02 12:38:58 +0200

    Remove Codeception tests

--- tests/functional/CreateUserFromConsoleCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$I = new FunctionalTester($scenario);
-$I->wantTo('create a new user from the command line');
-
-$I->runShellCommand('php artisan create:user superadmin@email.com "Super Admin" --admin');
-$I->seeInShellOutput('User created');
-$I->seeInDatabase('users', ['email' => 'superadmin@email.com', 'rights' => '["admin"]']);
