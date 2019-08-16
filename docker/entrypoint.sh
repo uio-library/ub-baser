074171ca0df5d9c30e243fafadbdc5f5bc112b91
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-08-16 12:33:20 +0200

    Update entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,11 @@
 
 set -e
 
+# ----------------------------------------------------------------------------
+# Certificate handling
+
 if [ -z "$SITE_CERTIFICATE" ] ; then
 	echo "Cannot start without a SITE_CERTIFICATE"
-	exit 1;
-fi
-if [ -z "$CA_CERTIFICATE" ] ; then
-	echo "Cannot start without a CA_CERTIFICATE"
 	exit 1;
 fi
 
@@ -19,29 +18,54 @@ fi
 echo "Storing certificates in $APACHE_CONFDIR"
 
 echo -e $SITE_CERTIFICATE > $APACHE_CONFDIR/site.crt
-echo -e $CA_CERTIFICATE > $APACHE_CONFDIR/ca.crt
 echo -e $SITE_CERTIFICATE_KEY > $APACHE_CONFDIR/site.key
 
-# Optimize Laravel. These commands depend on the environment,
-# so we cannot run them in the Dockerfile.
-php artisan route:cache
-php artisan config:cache
-php artisan view:cache
+if [ -z "$CA_CERTIFICATE" ] ; then
+	echo "Warning: No CA_CERTIFICATE specified"
+	echo -e $SITE_CERTIFICATE > $APACHE_CONFDIR/ca.crt
+else
+	echo -e $CA_CERTIFICATE > $APACHE_CONFDIR/ca.crt
+fi
 
-# Sanity check
+# ----------------------------------------------------------------------------
+# Fix permissions
 chmod -R a+rX .
+
+# ----------------------------------------------------------------------------
+# Optimize Laravel
+# (These commands depend on the environment, so we cannot run them in Dockerfile)
+
+if [ "$APP_ENV" == "production" ]; then
+	php artisan config:cache
+	php artisan route:cache
+	php artisan view:cache
+else
+	php artisan config:clear
+	php artisan route:clear
+	php artisan view:clear
+fi
+
+# ----------------------------------------------------------------------------
+# Use opcache if in production
 
 if [ "$APP_ENV" == "production" ]; then
 	echo "Copying opcache settings into $PHP_INI_DIR/conf.d/"
 	cp docker/opcache.ini $PHP_INI_DIR/conf.d/
 fi
 
+# ----------------------------------------------------------------------------
+# Wait for Postgres
+
 echo Waiting for ${DB_HOST}:${DB_PORT}
 docker/wait-for-it.sh ${DB_HOST}:${DB_PORT} -t 30
 
-# Run new migrations, if any
-php artisan migrate
+# ----------------------------------------------------------------------------
+# Run migrations, if any
 
-exec apache2-foreground "$@"
+php artisan migrate --force
+
+# ----------------------------------------------------------------------------
+# Apache time!
 
 echo "Startup took $SECONDS seconds"
+exec apache2-foreground "$@"
