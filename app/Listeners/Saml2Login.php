uio-library/ub-baser

commit 37c807b56ef0abb0b8810a0bfcc67e55ea234eb9

Improve login flow
Log first-time login, add user.show route, collapse local login

app/Listeners/Saml2Login.php
@@ -24,8 +24,6 @@ class Saml2Login
      */
     public function handle(Saml2LoginEvent $event)
     {
-        \Log::info('Received SAML login event.');
-
         $data = $event->getSaml2User();
         $attrs = $data->getAttributes();
 
@@ -33,6 +31,8 @@ class Saml2Login
         $user = User::where('saml_id', '=', $saml_id)->first();
 
         if (!is_null($user)) {
+            $user->last_login_at = Carbon::now();
+            $user->save();
             return \Auth::login($user);
         }
 

app/Http/Controllers/Admin/UserController.php
@@ -96,12 +96,38 @@ class UserController extends Controller
     {
         $user = $this->updateOrCreate($request);
 
+        $this->log(
+            'Opprettet ny bruker: <a href="%s">%s (%s)</a>.',
+            action('Admin\UserController@show', $user->id),
+            $user->name,
+            $user->email
+        );
+
         \Password::sendResetLink(['email' => $user->email], function (Message $message) {
             $message->subject('Velkommen til ub-baser');
         });
 
         return redirect()->action('Admin\UserController@index')
             ->with('status', 'En epost er sendt til brukeren med instruksjoner for å sette passord.');
+    }
+
+    /**
+     * Show a specific resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        $data = [
+            'user'   => $user,
+            'rights' => AuthServiceProvider::$rights,
+        ];
+
+        return response()->view('admin.user.show', $data);
     }
 
     /**
@@ -133,7 +159,14 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->updateOrCreate($request, $id);
+        $user = $this->updateOrCreate($request, $id);
+
+        $this->log(
+            'Oppdaterte bruker: <a href="%s">%s (%s)</a>.',
+            action('Admin\UserController@show', $user->id),
+            $user->name,
+            $user->email
+        );
 
         return redirect()->action('Admin\UserController@index')
             ->with('status', 'Brukeren ble lagret');

app/Http/Controllers/Auth/LoginController.php
@@ -54,8 +54,11 @@ class LoginController extends Controller
         if (\Auth::check()) {
             return redirect('/');
         }
+
+        $samlResponse = $request->session()->get('saml_response');
+
         return view('auth.saml_register', [
-            'data' => $request->session()->get('saml_response'),
+            'data' => $samlResponse,
         ]);
     }
 
@@ -77,6 +80,13 @@ class LoginController extends Controller
         $user->saml_session = $data['saml_session'];
 
         $user->save();
+
+        $this->log(
+            'Opprettet ny bruker fra UiO-innlogging: <a href="%s">%s (%s)</a>.',
+            action('Admin\UserController@show', $user->id),
+            $user->name,
+            $user->email
+        );
 
         \Auth::login($user);
 

app/Listeners/Saml2Logout.php
@@ -16,9 +16,6 @@ class Saml2Logout
      */
     public function handle(Saml2LogoutEvent $event)
     {
-        \Log::info('Received SAML logout event.');
-        // die('Got logout event');
-
         $user = \Auth::user();
         if ($user) {
             $user->saml_session = null;

resources/views/admin/user/show.blade.php
@@ -1,0 +1,49 @@
+@extends('layouts.master')
+
+@section('content')
+
+<nav aria-label="breadcrumb" class="mb-3">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ action('Admin\AdminController@index') }}">{{ trans('messages.admin') }}</a></li>
+        <li class="breadcrumb-item"><a href="{{ action('Admin\UserController@index') }}">{{ trans('messages.manageusers') }}</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{ $user->name }}</li>
+    </ol>
+</nav>
+
+@include('shared.errors')
+
+<div class="card my-3">
+
+    <div class="card-header">
+        Bruker #{{$user->id}}
+    </div>
+
+
+    <table class="table">
+        <tbody>
+            <tr>
+                <td>Navn</td>
+                <td>{{ $user->name }}</td>
+            </tr>
+            <tr>
+                <td>E-post</td>
+                <td>{{ $user->email }}</td>
+            </tr>
+            <tr>
+                <td>Registrert</td>
+                <td>{{ $user->created_at }}</td>
+            </tr>
+            <tr>
+                <td>Sist innlogget</td>
+                <td>{{ $user->last_login_at }}</td>
+            </tr>
+            <tr>
+                <td>Tilganger</td>
+                <td>{{ implode(', ', $user->rights) }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+
+@endsection

resources/views/auth/login.blade.php
@@ -5,21 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
+
             <div class="card">
                 <div class="card-header">{{ __('UiO-innlogging') }}</div>
                 <div class="card-body">
 
-                     <a class="btn btn-primary" href="{{ route('saml2_login', 'uio') }}">Logg inn sikkert med Weblogin</a>
+                    <a class="btn btn-primary" href="{{ route('saml2_login', 'uio') }}">
+                        <em class="fa fa-arrow-right"></em>
+                        Logg inn sikkert med Weblogin
+                    </a>
 
                 </div>
             </div>
 
 
 
-            <div class="card">
-                <div class="card-header">{{ __('Lokal innlogging') }}</div>
+            <div class="card my-3">
+                <div class="card-header"><a id="collapse1_toggle" href="#" onclick="$('#collapse1').toggleClass('collapse'); return false;">{{ __('Lokal innlogging (alternativ)') }}</a></div>
 
-                <div class="card-body">
+                <div class="card-body collapse" id="collapse1">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 

routes/web.php
@@ -19,15 +19,16 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 // Admin routes...
+Route::get('admin/users/{id}', 'Admin\UserController@show')->where('id', '[0-9]+');
 Route::group(['middleware' => 'admin'], function () {
 
     Route::get('admin', 'Admin\AdminController@index');
     Route::get('admin/users', 'Admin\UserController@index');
     Route::get('admin/users/create', 'Admin\UserController@create');
-    Route::get('admin/users/{id}', 'Admin\UserController@edit');
-    Route::post('admin/users/{id}', 'Admin\UserController@update');
+    Route::get('admin/users/{id}/edit', 'Admin\UserController@edit')->where('id', '[0-9]+');
+    Route::post('admin/users/{id}', 'Admin\UserController@update')->where('id', '[0-9]+');
     Route::post('admin/users', 'Admin\UserController@store');
-    Route::post('admin/users/delete/{id}', 'Admin\UserController@destroy');
+    Route::post('admin/users/delete/{id}', 'Admin\UserController@destroy')->where('id', '[0-9]+');
 
     Route::get('admin/pages', 'PageController@index');
 });

tests/selenium/pageobjects/loginpage.page.js
@@ -6,5 +6,13 @@ class LoginPage extends Page {
   open () {
     super.open('/login')
   }
+
+  loginAs(username, password) {
+    $('#collapse1_toggle').click()
+    $('input[name="email"]').setValue(username)
+    $('input[name="password"]').setValue(password)
+    $('button[type="submit"]').click()
+    browser.getUrl()
+  }
 }
 module.exports = new LoginPage()

commit 4f149b18923ebf49b5b6e4835ce241e697e1f925

Fix bug introduced in 37c807b56e

app/Listeners/Saml2Login.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use Aacotroneo\Saml2\Events\Saml2LoginEvent;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class Saml2Login