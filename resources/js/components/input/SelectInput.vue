176cc602d6aeeaf96552ef0ca0b205b2ecb19e1b
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-08-28 17:39:15 +0200

    Update frontend components to use selectize

--- resources/js/components/input/SelectInput.vue
+++ resources/js/components/input/SelectInput.vue
@@ -1,17 +1,31 @@
 <template>
-    <div>
-        <select :id="'select_' + name" :name="name">
-            <option :value="value.id" selected="selected">{{ value.label }}</option>
-        </select>
-        {{ value }}
+  <div>
+    <div v-if="!values.length">
+      <div v-if="failed" class="text-danger p-1">Det oppsto en feil. Prøv å laste siden på nytt.</div>
+      <div v-else class="p-1">Et øyeblikk...</div>
     </div>
+    <div v-else>
+      <selectize
+        :name="name"
+        :settings="settings"
+        :value="value"
+        @input="onInput($event)"
+      >
+        <option v-for="val in values" :key="val.id" :value="val.id">{{ val.label }}</option>
+      </selectize>
+    </div>
+  </div>
 </template>
 
 <script>
 import { get } from 'lodash/object'
+import Selectize from 'vue2-selectize'
 
 export default {
   name: 'select-input',
+  components: {
+    Selectize,
+  },
   props: {
     name: {
       type: String
@@ -20,47 +34,43 @@ export default {
       type: Object
     },
     value: {
-      type: Object
-    }
+      type: Number
+    },
   },
-  mounted () {
-    this.initSelectize()
-  },
-  beforeDestroy () {
-    this.destroySelectize()
-  },
-  methods: {
-    initSelectize () {
-      const url = get(this.schema, 'search.options.target')
-      $(`#select_${this.name}`).selectize({
+  data() {
+    return {
+      values: [],
+      failed: false,
+      settings: {
+        // Ref: https://github.com/selectize/selectize.js/blob/master/docs/usage.md
         create: false,
-        preload: true,
-
         valueField: 'id',
         labelField: 'label',
         searchField: 'label',
-
         openOnFocus: true,
         closeAfterSelect: true,
-
-        load: (query, callback) => {
-          this.$http.get(url, {
-            params: {
-              field: this.schema.key,
-              q: query
-            }
-          }).then(
-            res => callback(res.data),
-            err => callback([])
-          )
-        }
-      })
-    },
-    destroySelectize () {
-      // Note that selectize is not completely destroyed: https://github.com/selectize/selectize.js/issues/1257
-      // Should not be an issue for us since we're not making a SPA.
-      $(`#select_${this.name}`).selectize()[0].selectize.destroy()
+      },
     }
-  }
+  },
+  mounted () {
+    const url = get(this.schema, 'search.options.target');
+    this.$http.get(url, {
+      params: {
+        field: this.schema.key
+      }
+    }).then(res => {
+      this.values = res.data;
+    }).catch(err => {
+      this.failed = true;
+    })
+  },
+  beforeDestroy () {
+    //this.destroySelectize()
+  },
+  methods: {
+    onInput ($event) {
+      this.$emit('value', Number($event));
+    }
+  },
 }
 </script>
--- resources/js/components/input/TagsInput.vue
+++ resources/js/components/input/TagsInput.vue
@@ -1,31 +1,35 @@
 <template>
     <div>
-        <select multiple :id="'select_' + name" :name="name + '[]'">
-            <option v-for="item in value" :value="item" selected="selected">{{ item }}</option>
-        </select>
+      <selectize
+        multiple
+        :name="name + '[]'"
+        :settings="settings"
+        :value="value"
+        @input="onInput($event)"
+      >
+        <!-- eslint-disable-next-line vue/require-v-for-key -->
+        <option v-for="item in value" :value="item">{{ item }}</option>
+      </selectize>
     </div>
 </template>
 
 <script>
 import { get } from 'lodash/object'
+import Selectize from 'vue2-selectize'
 
 export default {
   name: 'tags-input',
+  components: {
+    Selectize,
+  },
   props: {
     name: String,
     schema: Object,
-    value: Array
+    value: Array,
   },
-  mounted () {
-    this.initSelectize()
-  },
-  beforeDestroy () {
-    this.destroySelectize()
-  },
-  methods: {
-    initSelectize () {
-      const url = get(this.schema, 'search.options.target')
-      $(`#select_${this.name}`).selectize({
+  data () {
+    return {
+      settings: {
         create: true,
         preload: true,
         labelField: 'value',
@@ -34,26 +38,26 @@ export default {
         openOnFocus: true,
         closeAfterSelect: true,
         load: (query, callback) => {
+          const url = get(this.schema, 'search.options.target')
           this.$http.get(url, {
             params: {
               field: this.schema.key,
-              q: query
-            }
-          }).then(
-            res => callback(res.data),
-            err => callback([])
-          )
+              q: query,
+            },
+          })
+            .then(res => callback(res.data))
+            .catch(() => callback([]))
         },
         render: {
-          option_create: (data, escape) => `<div class="create">Opprett «${escape(data.input)}»</div>`
-        }
-      })
-    },
-    destroySelectize () {
-      // Note that selectize is not completely destroyed: https://github.com/selectize/selectize.js/issues/1257
-      // Should not be an issue for us since we're not making a SPA.
-      $(`#select_${this.name}`).selectize()[0].selectize.destroy()
+          option_create: (data, escape) => `<div class="create">Opprett «${escape(data.input)}»</div>`,
+        },
+      },
     }
-  }
+  },
+  methods: {
+    onInput ($event) {
+      this.$emit('value', $event)
+    },
+  },
 }
 </script>
