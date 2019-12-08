902201007b2caee6aa9f939487637c71582337ff
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-12-08 21:27:01 +0100

    Improve error messages during migration
    Don't complain if log_entries table is not available yet

--- app/Logging/DatabaseLoggingHandler.php
+++ app/Logging/DatabaseLoggingHandler.php
@@ -5,19 +5,24 @@ namespace App\Logging;
 use App\LogEntry;
 use Illuminate\Support\Arr;
 use Monolog\Handler\AbstractProcessingHandler;
+use PDOException;
 
 class DatabaseLoggingHandler extends AbstractProcessingHandler
 {
     protected function write(array $record): void
     {
-        LogEntry::create([
-            'channel'    => Arr::get($record, 'channel'),
-            'message'    => Arr::get($record, 'message'),
-            'level'      => Arr::get($record, 'level'),
-            'level_name' => Arr::get($record, 'level_name'),
-            'context'    => $record['context'],
-            // 'extra'      => json_encode($record['extra']),
-            'time'   => $record['datetime']->format('Y-m-d G:i:s'),
-        ]);
+        try {
+            LogEntry::create([
+                'channel'    => Arr::get($record, 'channel'),
+                'message'    => Arr::get($record, 'message'),
+                'level'      => Arr::get($record, 'level'),
+                'level_name' => Arr::get($record, 'level_name'),
+                'context'    => $record['context'],
+                // 'extra'      => json_encode($record['extra']),
+                'time'   => $record['datetime']->format('Y-m-d G:i:s'),
+            ]);
+        } catch (PDOException $exception) {
+            // Ignore, the table might not have been created yet
+        }
     }
 }
