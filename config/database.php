f9ce462321d37ecf407d89e2b7bf6cc7853f34bc
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-12-08 23:06:41 +0100

    Refactor: Match environment variable names

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('POSTGRES_URL'),
+            'host' => env('POSTGRES_HOST', '127.0.0.1'),
+            'port' => env('POSTGRES_PORT', '5432'),
+            'database' => env('POSTGRES_DB', 'forge'),
+            'username' => env('POSTGRES_USER', 'forge'),
+            'password' => env('POSTGRES_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
