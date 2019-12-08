dbaa2e34e0b0373b61715639e6b58a20a19d1076
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2019-12-08 19:09:46 +0100

    Optimize bibsys indices

---
+++ database/migrations/2019_12_08_184522_optimize_bibsys_indices.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OptimizeBibsysIndices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create indices for use exact queries
+        foreach (['dokid', 'objektid', 'seriedokid', 'strekkode'] as $field) {
+            DB::unprepared("
+                DROP INDEX bibsys_search_${field}_idx
+            ");
+            DB::unprepared("
+                CREATE INDEX bibsys_search_${field}_idx ON bibsys_search(${field})
+            ");
+        }
+
+        // Create indices for LIKE queries: {field} ~~ lower(?*)
+        // Since we are targeting the C locale, we don't need text_pattern_ops .
+        foreach (['avdeling', 'samling', 'hyllesignatur'] as $field) {
+            DB::unprepared("
+                DROP INDEX bibsys_search_${field}_idx
+            ");
+            DB::unprepared("
+                CREATE INDEX bibsys_search_${field}_idx ON bibsys_search(lower(${field}))
+            ");
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
