c51ec0eacecd5849959522936e12734f56efca7c
Erik Winge <erik.winge@ub.uio.no>
2019-12-23 10:00:26 +0100

    ulov data

---
+++ initial-import/ulov.sql
@@ -0,0 +1,30 @@
+CREATE TABLE oversatte_lover (
+    lov_id          SERIAL  PRIMARY KEY,
+    dato DATE       NOT NULL,
+    nummer          SMALLINT,
+    dok_type TEXT   check(dok_type in ('lov','forskrift')),
+    sprak VARCHAR(15),
+    regdato DATE,
+    moddato DATE,
+    tittel TEXT,
+    kort TEXT,
+    --url TEXT,  --kan droppes?
+    note TEXT,
+    UNIQUE (dato, nummer)
+);
+
+CREATE TABLE oversettelser (
+    lov_id  NUMBER  NOT NULL REFERENCES oversatte_lover(lov_id),
+    dato DATE,
+    sprak VARCHAR(15),
+    regdato DATE,
+    moddato DATE,
+    tittel TEXT,
+    kort TEXT,
+    oversetter TEXT,
+    bibsys TEXT,
+    url TEXT,
+    note TEXT,
+    inote TEXT,
+    utgave TEXT,
+);
