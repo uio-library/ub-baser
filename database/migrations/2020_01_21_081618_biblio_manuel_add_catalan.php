ae67b82a76a0604f3101ab90b1c0b0db17682db3
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-01-21 09:55:10 +0100

    Bibliomanuel: Add Catalan language

---
+++ database/migrations/2020_01_21_081618_biblio_manuel_add_catalan.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BiblioManuelAddCatalan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::table('bases')
+            ->where(['id' => 'bibliomanuel'])
+            ->update([
+                'languages' => json_encode(['nb', 'es', 'ca', 'en']),
+                'name' => json_encode([
+                    'es' => 'Bibliografía sobre la obra de Manuel Vázquez Montalbán',
+                    'ca' => 'Bibliografia sobre l\'obra de Manuel Vázquez Montalbán',
+                    'en' => 'Bibliography on the works of Manuel Vázquez Montalbán',
+                    'nb' => 'Bibliografi over Manuel Vázquez Montalbáns utgivelser',
+                ]),
+            ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \DB::table('bases')
+            ->where(['id' => 'bibliomanuel'])
+            ->update([
+                'languages' => json_encode(['nb', 'es', 'en']),
+                'name' => json_encode([
+                    'es' => 'Bibliografía sobre la obra de Manuel Vázquez Montalbán',
+                    'en' => 'Bibliography on the works of Manuel Vázquez Montalbán',
+                    'nb' => 'Bibliografi over Manuel Vázquez Montalbáns utgivelser',
+                ]),
+            ]);
+    }
+}
