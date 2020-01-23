39c9805c644c1a55e870930130f8ac2651888e6c
Axmed <kurosay@gmail.com>
2020-01-23 11:05:38 +0100

    Lagt til oversettelse for engelsk

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -9,5 +9,14 @@ return [
     'confirmpassword' => 'Confirm password',
     'sendpasswordresetlink' => 'Send Password Reset Link',
     'language' => 'Language',
-
+    // DataTable
+    'first' => 'First',
+    'previous' => 'Previous',
+    'next' => 'Next',
+    'last' => 'Last',
+    'loading' => 'Loading...',
+    'nohits' => 'No hits',	
+    'advanced_search' => 'Advanced Search',
+    'login' => 'Login',
+    'logout' => 'Logout',
 ];
