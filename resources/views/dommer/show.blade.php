50a7c6661ee7d1d7a737fe4ce767a4e1d9015734
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-01-28 09:26:03 +0100

    Dommer: Legg til sletteknappen

--- resources/views/dommer/show.blade.php
+++ resources/views/dommer/show.blade.php
@@ -2,13 +2,44 @@
 
 @section('content')
 
-<p>
-	<a href="{{ URL::previous() }}"><em class="fa fa-arrow-circle-left"></em> Tilbake</a>
+@if ($record->trashed())
+    <div class="alert alert-danger">
+        {{ __('base.status.recordtrashed') }}
+    </div>
+@endif
+
+<div>
+	<a href="{{ URL::previous() }}" class="btn btn-link">
+        <em class="fa fa-arrow-circle-left"></em>
+        Tilbake
+    </a>
+
 	@can('dommer')
-		&nbsp;
-		<a href="{{ action('\App\Bases\Dommer\Controller@edit', $record->id) }}"><em class="fa fa-edit"></em> Rediger post</a>
-	@endcan
-</p>
+        <a href="{{ $base->action('edit', $record->id) }}" class="btn btn-link">
+            <em class="fa fa-edit"></em>
+            Rediger post
+        </a>
+
+        @if ($record->trashed())
+            <form style="display: inline-block" action="{{ $base->action('restore', $record->id) }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-link text-danger btn-xs">
+                    <em class="fa fa-undo"></em>
+                    Gjenopprett
+                </button>
+            </form>
+        @else
+            <form style="display: inline-block" action="{{ $base->action('destroy', $record->id) }}" method="post">
+                @csrf
+                <input type="hidden" name="_method" value="DELETE">
+                <button type="submit" class="btn btn-link text-danger btn-xs">
+                    <em class="fa fa-trash"></em>
+                    Slett
+                </button>
+            </form>
+        @endif
+    @endcan
+</div>
 
 <h2>
 	«{{ $record->navn }}»
