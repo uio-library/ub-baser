05292cfebf6fc7724e221c6117aab3bb67677b8d
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-02-10 01:24:25 +0100

    Litteraturkritikk fix: Display of person name in editor

--- app/Bases/Litteraturkritikk/PersonView.php
+++ app/Bases/Litteraturkritikk/PersonView.php
@@ -10,4 +10,38 @@ class PersonView extends Record
      * @var string
      */
     protected $table = 'litteraturkritikk_personer_view';
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['string_representation'];
+
+    public function normalizedName()
+    {
+        $names = [];
+        if ($this->etternavn) {
+            $names[] = $this->etternavn;
+        }
+        if ($this->fornavn) {
+            $names[] = $this->fornavn;
+        }
+        $nn = implode(', ', $names);
+        if ($this->fodt !== null) {
+            $nn .= ', ' . $this->fodt . '-' . ($this->dod ?: '');
+        }
+
+        return $nn;
+    }
+
+    public function getStringRepresentationAttribute()
+    {
+        return $this->normalizedName();
+    }
+
+    public function __toString()
+    {
+        return $this->normalizedName();
+    }
 }
