469bd7887895bc788b348f560f241e3f1f5848c8
Ahmed Mohammed <kurosay@gmail.com>
2020-02-13 17:24:09 +0100

    tilbake knapp i post visning for Letras

--- resources/views/letras/show.blade.php
+++ resources/views/letras/show.blade.php
@@ -10,32 +10,40 @@
             {{ __('base.status.recordtrashed') }}
         </div>
     @endif
-    @can('litteraturkritikk')
+    <div class="pb-3">
 
-        <a href="{{ $base->action('edit', $record->id) }}" class="btn btn-outline-primary">
-            <em class="fa fa-edit"></em>
-            Rediger post
+        <a href="{{ URL::previous() }}" class="btn btn-outline-primary">
+            <em class="fa fa-arrow-circle-left"></em>
+            Tilbake
         </a>
 
-        @if ($record->trashed())
-            <form style="display: inline-block" action="{{ $base->action('restore', $record->id) }}" method="post">
-                @csrf
-                <button type="submit" class="btn btn-outline-danger btn-xs">
-                    <em class="fa fa-undo"></em>
-                    Gjenopprett
-                </button>
-            </form>
-        @else
-            <form style="display: inline-block" action="{{ $base->action('destroy', $record->id) }}" method="post">
-                @csrf
-                <input type="hidden" name="_method" value="DELETE">
-                <button type="submit" class="btn btn-outline-danger btn-xs">
-                    <em class="fa fa-trash"></em>
-                    Slett
-                </button>
-            </form>
-        @endif
-    @endcan
+        @can('letras')
+
+            <a href="{{ $base->action('edit', $record->id) }}" class="btn btn-outline-primary">
+                <em class="fa fa-edit"></em>
+                Rediger post
+            </a>
+
+            @if ($record->trashed())
+                <form style="display: inline-block" action="{{ $base->action('restore', $record->id) }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-outline-danger btn-xs">
+                        <em class="fa fa-undo"></em>
+                        Gjenopprett
+                    </button>
+                </form>
+            @else
+                <form style="display: inline-block" action="{{ $base->action('destroy', $record->id) }}" method="post">
+                    @csrf
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="submit" class="btn btn-outline-danger btn-xs">
+                        <em class="fa fa-trash"></em>
+                        Slett
+                    </button>
+                </form>
+            @endif
+        @endcan
+    </div>
 
     @foreach ($schema->groups as $group)
         <h4 class="mt-4">{{ $group->label }}</h4>
