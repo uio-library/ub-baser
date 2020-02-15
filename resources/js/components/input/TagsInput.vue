9e54e20f60ce632cc84f0613dbbb62f02e8c1023
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-02-15 16:54:05 +0100

    Tags component: Add support for fixed list of values

--- resources/js/components/input/TagsInput.vue
+++ resources/js/components/input/TagsInput.vue
@@ -8,7 +8,10 @@
         :placeholder="placeholder"
         @input="onInput($event)"
       >
-        <option v-for="value in values" :key="value.id" :value="value.id">{{ value.prefLabel }}</option>
+        <option v-for="value in values"
+          :key="value.id"
+          :value="value.id"
+          :data-extra="value.extra">{{ value.prefLabel }}</option>
       </selectize>
     </div>
 </template>
@@ -36,18 +39,28 @@ export default {
     value: Array,
   },
   data () {
-    let values = this.value ? this.value.map(x => ({id: x, prefLabel: x})) : []
-    if (this.schema.values !== undefined) {
-      values = cloneDeep(this.schema.values)
+
+    let values = cloneDeep(this.schema.values) || []
+    values.forEach(val => val.extra = JSON.stringify({invalid: false}))
+    if (this.value) {
+      this.value.forEach(val => {
+        if (values.map(x => x.id).indexOf(String(val)) === -1) {
+          const invalid = this.schema.values && this.schema.values.length > 0
+          values.push({ id: val, prefLabel: val, extra: JSON.stringify({invalid: invalid}) })
+        }
+      })
     }
+
     let data = {
       values: values,
       placeholder: get(this.schema, 'edit.placeholder', ''),
       selectizeSettings: {
         create: get(this.schema, 'edit.allow_new_values', true),
-        valueField: 'prefLabel',
+        valueField: 'id',
+        labelField: 'prefLabel',
         searchField: ['prefLabel', 'altLabel'],
         closeAfterSelect: true,
+        dataAttr: 'data-extra',
         render: {
           option: this.renderOption.bind(this),
           item: this.renderItem.bind(this),
@@ -80,15 +93,25 @@ export default {
       if (data.altLabel) {
         return `<div class="option"><em>${escape(data.altLabel)}</em> → ${escape(data.prefLabel)}</div>`
       }
+      if (data.count) {
+        return `<div class="option">${escape(data.prefLabel)} (${escape(data.count)})</div>`
+      }
       return `<div class="option">${escape(data.prefLabel)}</div>`
     },
     renderItem(data, escape) {
+      if (data.invalid) {
+        return `<div class="text-danger">${escape(data.prefLabel)}</div>`
+      }
       return `<div>${escape(data.prefLabel)}</div>`
     },
     autocomplete(query, callback) {
       http.get(this.source().url.replace('{QUERY}', query))
         .then(res => {
-          callback(res.data.results)
+          callback(res.data.results.map(res => ({
+            id: res.prefLabel,
+            prefLabel: res.prefLabel,
+            count: res.count,
+          })))
         })
         .catch(err => {
           console.log('Autocomplete aborted', err)
