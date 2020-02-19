57db20a516e23d4a008b01eab86a8d03b62ef669
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-02-19 10:05:24 +0100

    CKEditor: Add footnote style

--- resources/js/components/PageEditor.vue
+++ resources/js/components/PageEditor.vue
@@ -139,6 +139,18 @@ export default {
             { model: 'heading3', view: 'h3', title: 'Heading 2', class: 'ck-heading_heading3' },
             { model: 'heading4', view: 'h4', title: 'Heading 3', class: 'ck-heading_heading4' },
             { model: 'heading5', view: 'h5', title: 'Overskrift 4', class: 'ck-heading_heading5' },
+            {
+                model: 'smallParagraph',
+                view: {
+                    name: 'p',
+                    classes: 'footnote'
+                },
+                title: 'Fotnotetekst',
+                class: 'ck-heading_small_paragraph',
+
+                // It needs to be converted before the standard 'paragraph'.
+                converterPriority: 'high'
+            }
           ],
         },
 
