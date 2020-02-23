uio-library/ub-baser

commit 17c0c40988a6987e79349f6fbcd23cde7187f4bb

SearchForm: Use same type when adding a new field

resources/js/components/SearchForm.vue
@@ -86,15 +86,6 @@ export default {
       return fields
     },
 
-    firstSearchField () {
-      for (let i = 0; i < this.allFields.length; i++) {
-        if (this.allFields[i].search.enabled) {
-          return this.allFields[i]
-        }
-      }
-      throw new Error('Found no search fields!')
-    },
-
   },
 
   data () {
@@ -111,10 +102,10 @@ export default {
 
   methods: {
     addField () {
-      console.log(this.firstSearchField)
+      const lastField = this.query[this.query.length - 1]
       this.query.push({
-        field: this.firstSearchField.key,
-        operator: get(this.firstSearchField, 'search.operators.0', 'contains'),
+        field: lastField.field,
+        operator: lastField.operator,
         value: '',
       })
     },

commit 5b2e270577e4cf64816bd1eb4f8dd5bb56fd27ec

Fix SearchForm
Broke in 17c0c40988a6987e79349f6fbcd23cde7187f4bb

resources/js/components/SearchForm.vue
@@ -104,12 +104,13 @@ export default {
 
   methods: {
     addField () {
-      const lastField = this.query[this.query.length - 1]
-      this.query.push({
-        field: lastField.field,
-        operator: lastField.operator,
-        value: '',
-      })
+      if (this.query.length) {
+        const lastField = this.query[this.query.length - 1]
+        this.query.push({ field: lastField.field, operator: lastField.operator, value: '' })
+      } else {
+        const enabled = this.allFields.filter(field => field.search.enabled)
+        this.query.push({ field: enabled[0].key, operator: enabled[0].search.operators[0], value: '' })
+      }
     },
 
   },