b2c873f70cb124dbb23a142af1c80ba20a805be7
Ahmed Mohammed <kurosay@gmail.com>
2020-02-25 17:41:21 +0100

    Lagt til ny felt for Letras, Utgitt i

---
+++ database/migrations/2020_02_24_094114_add_utgitt_i_to_letras.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUtgittIToLetras extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('letras', function (Blueprint $table) {
+            $table->string('utgitti')->nullable();
+            $table->string('utgitti2')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('letras');
+    }
+}
