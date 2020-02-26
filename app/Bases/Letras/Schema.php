9d186e37ff2caff9aa72b07f356246173f9d5f8e
Ahmed Mohammed <kurosay@gmail.com>
2020-02-26 14:41:25 +0100

    rettet Letras schema, fjernet default search valg

--- app/Bases/Letras/Schema.php
+++ app/Bases/Letras/Schema.php
@@ -46,60 +46,36 @@ class Schema extends BaseSchema
                     [
                         "key" => "forfatter",
                         "type" => "autocomplete",
-                        "search" => [
-                            "placeholder" => "Name of author",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Land
                     [
                         "key" => "land",
                         "type" => "autocomplete",
-                        "search" => [
-                            "placeholder" => "Country name",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Tittel
                     [
                         "key" => "tittel",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Title",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Utgitt i
                     [
                         "key" => "utgitti",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Utgitt i",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Utgivelsesår
                     [
                         "key" => "utgivelsesaar",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Published year",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Sjanger
                     [
                         "key" => "sjanger",
                         "type" => "autocomplete",
-                        "search" => [
-                            "placeholder" => "Genre",
-                            "type" => "simple",
-                        ],
                     ],
                 ],
             ],
@@ -112,10 +88,6 @@ class Schema extends BaseSchema
                     [
                         "key" => "oversetter",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Name of translator",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Tittel
@@ -133,40 +105,24 @@ class Schema extends BaseSchema
                     [
                         "key" => "utgitti2",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Utgitt i",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Utgivelsessted
                     [
                         "key" => "utgivelsessted",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Place of publication",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Utgivelsesår
                     [
                         "key" => "utgivelsesaar2",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Year of publication",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Forlag
                     [
                         "key" => "forlag",
                         "type" => "simple",
-                        "search" => [
-                            "placeholder" => "Publisher",
-                            "type" => "simple",
-                        ],
                     ],
 
                     // Forord/etterord
@@ -174,7 +130,6 @@ class Schema extends BaseSchema
                         "key" => "foretterord",
                         "type" => "simple",
                         "search" => [
-                            "placeholder" => "Foreword",
                             "advanced" => true,
                         ],
                     ],
@@ -182,7 +137,7 @@ class Schema extends BaseSchema
                     // Språk
                     [
                         "key" => "spraak",
-                        "type" => "simple",
+                        "type" => "autocomplete",
                         "search" => [
                             "placeholder" => "Language",
                             "type" => "simple",
