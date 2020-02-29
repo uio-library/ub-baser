d45cec31405e29199c99154c5cb8ec357bff8f0c
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-02-29 23:05:30 +0100

    Extract common view code into base.index

--- resources/views/bibsys/index.blade.php
+++ resources/views/bibsys/index.blade.php
@@ -1,34 +1,5 @@
-@extends('bibsys.layout')
+@extends('base.index')
 
-@section('content')
-
-    @can('bibsys')
-        <p>
-            <a href="{{ $base->pageAction('intro', 'edit') }}"><em class="fa fa-edit"></em> Rediger introtekst</a>
-        </p>
-    @endif
-
-    {!! $intro !!}
-
-     <div class="panel panel-default">
-        <div class="panel-body">
-            <search-form
-                :initial-query="{{ json_encode($processedQuery) }}"
-                :schema="{{ json_encode($schema) }}"
-                :settings="{{ json_encode($settings) }}"
-                :advanced-search="{{ json_encode($advancedSearch) }}"
-            ></search-form>
-        </div>
-     </div>
-
-     <data-table
-         v-once
-         prefix="{{ $base->id }}"
-         base-url="{{ $base->action('index') }}"
-         :schema="{{ json_encode($schema) }}"
-         :default-columns="{{ json_encode($defaultColumns) }}"
-         :order="{{ json_encode($order) }}"
-         :query="{{ json_encode($query, JSON_FORCE_OBJECT) }}"
-     ></data-table>
-
+@section('base_actions')
 @endsection
+
