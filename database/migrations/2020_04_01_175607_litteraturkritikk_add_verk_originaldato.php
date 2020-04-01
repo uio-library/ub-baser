4dfd02fb3a86c0678f0037325fbed8d293d9cbd7
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-04-01 19:49:03 +0200

    NLK: Legg til verk_originaldato

---
+++ database/migrations/2020_04_01_175607_litteraturkritikk_add_verk_originaldato.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Traits\MigrationHelper;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LitteraturkritikkAddVerkOriginaldato extends Migration
+{
+    use MigrationHelper;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('litteraturkritikk_records', function (Blueprint $table) {
+            $table->text('verk_originaldato')->nullable();
+        });
+
+        $this->dropMaterializedView('litteraturkritikk_records_search');
+        $this->createView('litteraturkritikk_records_search', 10);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->dropMaterializedView('litteraturkritikk_records_search');
+
+        Schema::table('litteraturkritikk_records', function (Blueprint $table) {
+            $table->dropColumn('verk_originaldato');
+        });
+
+        $this->createView('litteraturkritikk_records_search', 9);
+    }
+}
