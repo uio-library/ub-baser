e5d52c10368c8df5767d4c469451c88743a2c7c2
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-05-24 21:06:30 +0200

    Test GitHub Actions workflow

--- dev.sh
+++ dev.sh
@@ -95,7 +95,7 @@ if [[ $CMD == "up -d"* ]]; then
     SECONDS=0
     until $(curl --output /dev/null --silent --head --fail $APP_HOST); do
         echo "[dev.sh]] Still not ready"
-        docker-compose -f "docker/compose/${APP_ENV}.yml" -p "${PROJECT_NAME}" exec -T app sh -c 'tail storage/logs/*.log || echo "No log files yet"'
+        docker-compose -f "docker/compose/${APP_ENV}.yml" -p "${PROJECT_NAME}" exec -T app sh -c 'tail -n 100 storage/logs/*.log || echo "No log files yet"'
         sleep 5
     done
     duration=$SECONDS
