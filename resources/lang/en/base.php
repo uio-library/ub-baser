93e0aa382f37914e600b801c411cfa11d359b361
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2020-09-16 15:55:01 +0200

    Add missing English translations

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -1,1 +1,26 @@
 <?php
+
+return [
+    'id' => 'ID',
+    'basepath' => 'Path',
+    'languages' => 'Language',
+    'default_language' => 'Default language',
+    'namespace' => 'Namespace',
+
+    'error' => [
+        'notfound' => 'No base «:name» was found',
+        'recordnotfound' => 'Record not found',
+        'recordtrashed' => 'The record is deprecated.  Log in to view it.',
+    ],
+    'notification' => [
+        'recordtrashed' => 'Record deprecated. It will not be visible in search results and not for non-logged in users.',
+        'recordrecovered' => 'Record restored.',
+        'recordupdated' => 'Record updated.',
+        'recordcreated' => 'Record created.',
+    ],
+    'status' => [
+        'recordtrashed' => 'This record is deprecated. It\'s not displayed to non-logged in users, and does not show up in search results.',
+        'unpublished' => 'This record is unpublished.',
+        'unpublished_loggedin' => 'This record is unpublished. It\'s not shown unless you\'re logged in.',
+    ],
+];
