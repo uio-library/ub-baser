c81d1d3dc55fe9e8f2c26692d2d1f604e078ebd9
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2021-01-04 14:20:00 +0100

    Fix routing with Laravel 8

--- routes/channels.php
+++ routes/channels.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Broadcast;
+
 /*
 |--------------------------------------------------------------------------
 | Broadcast Channels
@@ -9,6 +12,7 @@
 | used to check if an authenticated user can listen to the channel.
 |
 */
+
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
