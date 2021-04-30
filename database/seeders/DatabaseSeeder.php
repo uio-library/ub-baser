6a14a67d2a519a06f89c96ed42f406361ef815c8
Ahmed Mohammed <kurosay@gmail.com>
2021-04-30 19:13:54 +0200

    database seeder files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call(DommerTableSeeder::class);
         $this->call(LetrasTableSeeder::class);
         $this->call(BibliomanuelTableSeeder::class);
+        $this->call(BibliofremmespraakTableSeeder::class);
         $this->call(OpesTableSeeder::class);
         $this->call(PagesTableSeeder::class);
         $this->call(BibsysTableSeeder::class);
