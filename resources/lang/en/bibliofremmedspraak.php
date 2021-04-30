89c0ef0409220840c1770919afb99a9adfba611e
Ahmed Mohammed <kurosay@gmail.com>
2021-05-01 00:45:57 +0200

    språk filer

---
+++ resources/lang/en/bibliofremmedspraak.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    // Meta
+    'id' => 'ID',
+    'created_at' => 'Created',
+    'updated_at' => 'Last modified',
+
+    // Felt
+    'forfatter' => 'Author',
+    'tittel' => 'Title',
+    'tidsskrift' => 'Journal',
+    'antologi' => 'Antology',
+    'angitti' => '?',
+    'utgivelsessted' => 'Place of publication',
+    'nummer' => 'Number',
+    'utgivelsesaar' => 'Year of publication',
+    'forlag' => 'Publisher',
+    'emne' => 'Subject',
+    'type' => 'Type',
+    'dato' => 'Date',
+    'utgiver' => '?',
+    'ansvarsangivelse' => '?',
+    'eissn' => 'E-ISSN',
+    'issn' => 'ISBN',
+    'isbn' => 'ISBN',
+    'eisbn' => 'E-ISBN',
+    'boktittel' => 'Book title',
+    'nettsted' => 'Website',
+    'spraak' => 'Language',
+    'url' => 'URL',
+    'sidetall' => 'Page number',
+    'digitalisering' => 'Digitalization',
+
+    // Grensesnitt
+    'footer_line1' => 'Operated by :publisher.',
+    'ubo' => 'University of Oslo Library',
+    'footer_line2' => 'For questions or feedback, contact :contact.',
+];
---
+++ resources/lang/nb/bibliofremmedspraak.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    // Meta
+    'id' => 'ID',
+    'created_at' => 'Opprettet',
+    'updated_at' => 'Sist endret',
+
+    // Felt
+    'forfatter' => 'Forfatter',
+    'tittel' => 'Tittel',
+    'tidsskrift' => 'Tidsskrift',
+    'antologi' => 'Antologi',
+    'angitti' => 'Angitt i',
+    'utgivelsessted' => 'Utgivelsessted',
+    'nummer' => 'Nummer',
+    'utgivelsesaar' => 'Utgivelsesår',
+    'forlag' => 'Forlag',
+    'emne' => 'Emne',
+    'type' => 'Type',
+    'dato' => 'Dato',
+    'utgiver' => 'Utgiver',
+    'ansvarsangivelse' => 'Ansvarsangivelse',
+    'eissn' => 'E-ISSN',
+    'issn' => 'ISBN',
+    'isbn' => 'ISBN',
+    'eisbn' => 'E-ISBN',
+    'boktittel' => 'Boktittel',
+    'nettsted' => 'Nettsted',
+    'spraak' => 'Språk',
+    'url' => 'URL',
+    'sidetall' => 'Sidetall',
+    'digitalisering' => 'Digitalisering',
+
+    // Grensesnitt
+    'footer_line1' => 'Basen driftes av :publisher.',
+    'ubo' => 'Universitetsbiblioteket i Oslo',
+    'footer_line2' => 'For spørsmål og innspill, kontakt :contact.',
+];
