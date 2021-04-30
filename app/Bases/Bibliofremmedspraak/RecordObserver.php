bfa1a3324e1a1551c2621d9c2de54d3743be8095
Ahmed Mohammed <kurosay@gmail.com>
2021-04-30 03:57:55 +0200

    add recordobserver and initial import tsv file

---
+++ app/Bases/Bibliofremmedspraak/RecordObserver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Bases\bibliofremmedspraak;
+
+class RecordObserver
+{
+    protected function refreshView()
+    {
+        // Refresh view
+        \DB::unprepared('REFRESH MATERIALIZED VIEW CONCURRENTLY bibliofremmedspraak_view');
+    }
+
+    /**
+     * Handle the app bases bibliofremmedspraak record "created" event.
+     *
+     * @param Record $record
+     * @return void
+     */
+    public function created(Record $record)
+    {
+        $this->refreshView();
+    }
+
+    /**
+     * Handle the app bases bibliofremmedspraak record "updated" event.
+     *
+     * @param Record $record
+     * @return void
+     */
+    public function updated(Record $record)
+    {
+        $this->refreshView();
+    }
+
+    /**
+     * Handle the app bases bibliofremmedspraak record "deleted" event.
+     *
+     * @param Record $record
+     * @return void
+     */
+    public function deleted(Record $record)
+    {
+        $this->refreshView();
+    }
+
+    /**
+     * Handle the app bases bibliofremmedspraak record "restored" event.
+     *
+     * @param Record $record
+     * @return void
+     */
+    public function restored(Record $record)
+    {
+        $this->refreshView();
+    }
+
+    /**
+     * Handle the app bases bibliofremmedspraak record "force deleted" event.
+     *
+     * @param Record $record
+     * @return void
+     */
+    public function forceDeleted(Record $record)
+    {
+        $this->refreshView();
+    }
+}
